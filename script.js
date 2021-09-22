d4bf9817ff0e8197a927bf0632bf0661bdef35b6
edlopez000 <edlopez000>
2021-09-21 22:42:29 -0400

    Basic functionality added

--- script.js
+++ script.js
@@ -18,27 +18,61 @@ window.addEventListener('load', () => {
   const fullIMG = new Image();
   fullIMG.src = 'template.png';
 
+  // Creates the context for the canvas
   const canvas = createHiPPICanvas(fullIMG.width, fullIMG.height);
   const ctx = canvas.getContext('2d');
 
-  // Populates the canvas with the image
-  ctx.drawImage(fullIMG, 0, 0);
+  // Populates the canvas with the image and also clears the canvas
+  function clearAndDraw() {
+    ctx.drawImage(fullIMG, 0, 0);
+  }
 
   // Sets the canvas font in pixels and the font-family
   ctx.font = '50px serif';
 
-  // Trainee name
-  const traineeName = 'Edward Lopez-Ramos';
-  const nameWidth = ctx.measureText(traineeName).width;
-  const nameCenter = fullIMG.width / 2 - nameWidth / 2;
-  ctx.fillText(traineeName, nameCenter, 750);
+  function writeName(name) {
+    const NAME_Y_COORDINATE = 750;
+    const nameWidth = ctx.measureText(name).width;
+    const nameCenter = fullIMG.width / 2 - nameWidth / 2;
+    ctx.fillText(name, nameCenter, NAME_Y_COORDINATE);
+  }
 
-  // Date of completion
-  ctx.fillText('09/12/2021', 975, 1075);
+  function writeDate(date) {
+    const DATE_X_COORDINATE = 975;
+    const DATE_Y_COORDINATE = 1075;
+    const dateArr = date.split('-');
+    const finalDate = `${dateArr[1]}/${dateArr[2]}/${dateArr[0]}`;
+    ctx.fillText(finalDate, DATE_X_COORDINATE, DATE_Y_COORDINATE);
+  }
 
-  // Displays the changed image
-  // Can be removed or changed into a preview image before download
-  const finalProduct = document
-    .getElementById('completedImage')
-    .appendChild(canvas);
+  function downloadCanvas(canvasObj, nameVar) {
+    const link = document.createElement('a');
+    const name = nameVar.split(' ').join('_');
+    link.download = `${name}_GIS_Certificate.png`;
+    link.href = canvasObj.toDataURL();
+    link.click();
+  }
+
+  const form = document.querySelector('form');
+  form.addEventListener('submit', () => {
+    // eslint-disable-next-line no-restricted-globals
+    event.preventDefault();
+
+    const nameInput = document.querySelector('input[name=nameInput]');
+    const nameValue = nameInput.value;
+
+    const dateInput = document.querySelector('input[name=dateInput]');
+    const dateValue = dateInput.value;
+
+    clearAndDraw();
+
+    writeName(nameValue);
+    writeDate(dateValue);
+
+    downloadCanvas(canvas, nameValue);
+
+    // const finalProduct = document
+    //   .getElementById('completedImage')
+    //   .appendChild(canvas);
+  });
 });
